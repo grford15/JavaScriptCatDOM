42a1b2bffaa95f834cc5888333eb5c660d4bfb06
Greg Rutherford <g.rutherford10@gmail.com>
2018-07-30 19:45:48 +0100

    MVP complete

--- public/app.js
+++ public/app.js
@@ -1,5 +1,8 @@
 var CAT_ARRAY = [
-  {name: "Jim", food: "Cheese", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkR859WIv6s6NKRbcBCNXKZanLy06xYujmPYSU_vZSPIdu7gyB"}
+  {name: "Jim", food: "Cheese", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkR859WIv6s6NKRbcBCNXKZanLy06xYujmPYSU_vZSPIdu7gyB"},
+  {name: "Boba", food: "Sock fluff", image: "http://66.media.tumblr.com/d1f01bbe0150fda0c40d2151c5eaeac8/tumblr_odlqqskjj61v9cejwo1_400.jpg"},
+  {name: "Barnaby", food: "Tuna", image: "https://68.media.tumblr.com/88d0fcf2b84a7b098dda839130597569/tumblr_okuo4teiql1uhevdso1_1280.jpg"},
+  {name: "Max", food: "Whiskas Temptations", image: "http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg"}
 
 ];
 
@@ -37,6 +40,7 @@ var createCatPic = function(catPic){
 
   var catImage = document.createElement('img');
   catImage.src = catPic;
+  catImage.classList.add('img');
 
   catLi.appendChild(catImage);
   return catLi;
